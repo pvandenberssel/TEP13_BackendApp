01623af8800fa18a5777b3123875e7d5e91b83a5
Jappedonquixote <jasperlustig@gmail.com>
2019-03-11 11:00:38 +0100

    gettters en setters toegevoed

--- BackendApplication/src/main/java/com/example/TEP13/Backend/Application/domain/User.java
+++ BackendApplication/src/main/java/com/example/TEP13/Backend/Application/domain/User.java
@@ -21,7 +21,39 @@ public class User {
         return id;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
     public void setId(long id) {
         this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
