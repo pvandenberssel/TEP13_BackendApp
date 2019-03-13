f13037907e846a112040b15fa889ed66baf47b98
Pim van den Bersselaar <pim.van.den.bersselaar@youngcolfield.nl>
2019-03-13 13:19:04 +0100

    commit

---
+++ BackendApplication/src/test/java/com/example/TEP13/Backend/Application/controller/CardServiceTest.java
@@ -0,0 +1,38 @@
+package com.example.TEP13.Backend.Application.controller;
+
+import com.example.TEP13.Backend.Application.domain.Card;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CardServiceTest {
+    @InjectMocks
+    private CardService cardService;
+    @Mock
+    private CardRepository cardRepository;
+
+    private Card card = new Card();
+
+    @Test
+    public void testSave(){
+        Card result = new Card();
+        Mockito.when(this.cardRepository.save(card)).thenReturn(result);
+        Assert.assertEquals(result,this.cardRepository.save(card));
+    }
+
+    @Test
+    public void testFindAll(){
+        ArrayList<Card> cards = new ArrayList<Card>();
+        Mockito.when(this.cardRepository.findAll()).thenReturn(cards);
+        Assert.assertEquals(cards,this.cardRepository.findAll());
+    }
+
+}
